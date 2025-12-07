7616f1edaa2ed774d6c5d443e24dfe8d293bb731
sumirai03 <121746663+sumirai03@users.noreply.github.com>
2025-12-07 23:10:06 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "envougue"
+  type        = string
+}
+
+variable "region" {
+  description = "us-central1"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "bucket_name" {
+  description = "envougue"
+  type        = string
+}
